af3e1aa3154994eea83b6e478b05349b333aa155
psleepy <dotsleepyisaloser@gmail.com>
2018-02-05 01:21:55 -0500

    uploading text landscape and sappho stuff

---
+++ js/text-landscape-sappho.js
@@ -0,0 +1,8 @@
+$(function(){
+  $('nav a').click(function(){
+      $('html, body').animate({
+          scrollTop: $( $(this).attr('href') ).offset().top
+      }, 500);
+      return false;
+  });
+});
